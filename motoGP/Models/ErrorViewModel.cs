338ad6726bb1cdb7976fefed32db3baa4e299751
Wouter Eilers <c1042450@hik.be>
2020-02-15 16:02:49 +0100

    Add project files.

---
+++ motoGP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace motoGP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
